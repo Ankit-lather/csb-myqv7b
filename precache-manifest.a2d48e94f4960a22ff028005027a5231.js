2f297a6cc4fa51cf04bd1dac0e2848410dc3f494
Code Sandbox <hello@codesandbox.io>
2022-06-03 17:52:06 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.a2d48e94f4960a22ff028005027a5231.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d37f760fbfac635a36dc32e3e9f9398e",
+    "url": "https://ankit-lather.github.io/csb-myqv7b/index.html"
+  },
+  {
+    "revision": "4aa7c60bd970ef4afafa",
+    "url": "https://ankit-lather.github.io/csb-myqv7b/static/js/2.5b73a998.chunk.js"
+  },
+  {
+    "revision": "a606b4831720897a4e39",
+    "url": "https://ankit-lather.github.io/csb-myqv7b/static/js/main.0cbea0c0.chunk.js"
+  },
+  {
+    "revision": "8ad59464976b1051b033",
+    "url": "https://ankit-lather.github.io/csb-myqv7b/static/js/runtime-main.b6f5530e.js"
+  }
+]);
